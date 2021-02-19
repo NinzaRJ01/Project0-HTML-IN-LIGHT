8ed435de2242b96cf0e0e83704b8cebdcae9e59b
NinzaRJ01 <leorajat.ray@gmail.com>
2021-02-19 15:57:37 +0530

    Script : MainContentjs Added

---
+++ Scripts/MenuControl.js
@@ -0,0 +1,18 @@
+let menu = document.querySelector(".Menu");
+let isOpened = true;
+let menuContentList = document.querySelectorAll(".MenuContent");
+menu.addEventListener('click',()=>{
+    
+    if(isOpened){
+        for(let i=0;i<menuContentList.length;i++){
+            menuContentList[i].style.display="none";
+        }
+        isOpened=false;
+    }
+    else{
+        for(let i=0;i<menuContentList.length;i++){
+            menuContentList[i].style.display="flex";
+        }
+        isOpened=true;
+    }
+})
